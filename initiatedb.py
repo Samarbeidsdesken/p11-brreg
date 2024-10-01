c4cd73a9702c6a88dc8fd07a24e9e254995c1b26
Øivind Skjervheim <oskjervheim@gmail.com>
2024-10-01 15:49:40 +0200

    uncommented lines

--- initiatedb.py
+++ initiatedb.py
@@ -1,6 +1,6 @@
 
-# from dbinitiate.dbcreate_tables import create_table_bostyrer
-# from dbinitiate.dbcreate_tables import create_table_konkurser
+from dbinitiate.dbcreate_tables import create_table_bostyrer
+from dbinitiate.dbcreate_tables import create_table_konkurser
 
 from dbinitiate.dbcreate_tables import create_table_enheter
 from dbinitiate.dbcreate_tables import create_table_forretningsadresse
@@ -9,8 +9,8 @@ from dbinitiate.dbcreate_tables import create_table_roller
 
 
 if __name__ == '__main__':
-    # create_table_konkurser();
-    # create_table_bostyrer()
+    create_table_konkurser()
+    create_table_bostyrer()
 
     create_table_enheter()
     create_table_roller()
