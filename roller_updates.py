935264c4a408494298339e6ca18fb55bec83c643
Øivind Skjervheim <oskjervheim@gmail.com>
2024-10-01 08:12:34 +0200

    cleaned and added some documentation

--- roller_updates.py
+++ roller_updates.py
@@ -1,27 +1,34 @@
 
+# Functions for communicating with brreg API
 from roller import update_roller
 from roller import roller
+
+# Functions for writing data to database
 from dbfunctions.dbinsert_roller import insert_roller
 from dbfunctions.dbupdate_roller import insert_roller_update
-# import matplotlib.pyplot as plt
+
+# Others
 import json
 from datetime import datetime
 
-
+# Get companies where the roles have been updated
 updated_orgs = update_roller.get_updated_roles()
 
-
+# Loop through all 
 for org in updated_orgs:
     # print(org)
     orgnr = org['data']['organisasjonsnummer']
-
+    
+    # Get the new company roles
     response_dict = roller.get_company_roles(orgnr)
 
+    # If company roles
     if response_dict:
+        # parse roles from dict
         enhet_roller = roller.get_roles(response_dict)
 
         if enhet_roller:
-
+            # make a tuple with orgnr and a json string
             data = [(orgnr, json.dumps(enhet_roller))]
 
             try:
